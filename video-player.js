9e402c65de7a204338fab0e67bbbc8ff607ee967
Jousethlc <98976120+Jousethlc@users.noreply.github.com>
2022-02-03 12:57:44 -0500

    Update video-player.js

--- video-player.js
+++ video-player.js
@@ -4,7 +4,6 @@ var player = videojs('videoPlayer',{
 	loop: true,
 	fluid: true,
 	aspectRatio: '4:3',
-	playbackRates: [0.25, 0.5, 1, 1.5, 2,],
 	plugins: {
 		hotkeys: {
            enableModifiersForNumbers: false,
